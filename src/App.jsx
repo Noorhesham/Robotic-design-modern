dab881e5dd10ec79fbfd3623444511ab88a3facf
Noorhesham <noordragon2004@gmail.com>
2024-05-04 21:14:54 -0700

    dasda

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Login from "./components/Login";
 import Signup from "./pages/Signup";
 
 const App = () => {
-  return <BrowserRouter>
-    <Routes>
-      <Route  path="/" element={<Main/>}/>
-      <Route path="/login" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-    </Routes>
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+          <Route path="login" element={<Login />} />
+          <Route path="signup" element={<Signup />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
